304380d1da102435c30e6896fab752f35e14479a
Bruno Bouvet <bouvet.bruno@gmail.com>
2016-07-22 17:41:41 +0200

    Fini les exercices 1 a 3

---
+++ ex3.rb
@@ -0,0 +1,26 @@
+#input text 
+puts "I will now count my chickens:"
+
+# Calculate the number of hens
+puts "Hens #{25 + 30 / 6}"
+# Addition the number of eggs
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# Addition the number of eggs
+puts "Now I will count the eggs:"
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#input text 
+puts "Is it true that 3 + 2 < 5 - 7?"
+#définie le calcule 
+puts 3 + 2 < 5 - 7
+#vérifie la valeur et dit si il est vrai ou fausse
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+#input text 
+puts "Oh, that's why it's false."
+#input text
+puts "How about some more."
+#vérifie les paramètres de l'équations
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
